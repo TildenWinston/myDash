26a9bb19f318add9d52266bf3a66d1c7b6a7f429
DonJoel <jt3bk@virginia.edu>
2020-02-17 14:49:31 -0500

    chore: v1 of to do list

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "toDoList.apps.TodolistConfig",
+    "todo.apps.TodoConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
