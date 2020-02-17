f705225103132e13e0505e2433bf1f949c932dde
DonJoel <jt3bk@virginia.edu>
2020-02-16 22:31:59 -0500

    chore: working app

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "toDoList.apps.TodolistConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
