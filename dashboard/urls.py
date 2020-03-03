34e1edccb6e427f9296cedd5064d090226dc3fac
Conor Frymire <cjf6ac@virginia.edu>
2020-03-03 17:59:06 -0500

    added encrypted api key

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,17 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.contrib.auth import logout
-<<<<<<< HEAD
-
-
-=======
->>>>>>> ab50740b60181bb2e3396ef8bbb16aeb0aaa0cd0
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')),
     path('', include('pages.urls')),
     path('users/', include('users.urls')), 
-    path('users/', include('django.contrib.auth.urls')),
+    #path('users/', include('django.contrib.auth.urls')),
     path('accounts/', include('allauth.urls'))
 ]
