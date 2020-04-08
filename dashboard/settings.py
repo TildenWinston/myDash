539776ba0c5d6f078d21a22d70901d091dcdabfb
DonJoel <jt3bk@virginia.edu>
2020-04-08 12:44:00 -0400

    chore: fixing tables

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -14,7 +14,10 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+if 'I_AM_HEROKU' in os.environ:
+    # Configure Django App for Heroku.
+    import django_heroku
+    django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
