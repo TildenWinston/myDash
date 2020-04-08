76a346c04ba12c813cb215c9b4efe023bbc0ccbf
DonJoel <jt3bk@virginia.edu>
2020-04-08 12:26:38 -0400

    chore: adding an allowed host

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xy4(+@z$0sea7g=i#%w+^u5c3dlk2m7!e3h0dm5nj!=y!tpsio'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://dashboard-devs.herokuapp.com/']
 
 
 # Application definition
