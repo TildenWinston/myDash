2771b8d746b6ed4e61f5555838e88d9ffca14737
Conor Frymire <cjf6ac@virginia.edu>
2020-04-14 18:12:55 -0400

    correctly added calendar to settings

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,10 +8,7 @@ from django.shortcuts import redirect
 def my_logout(request):
     logout(request)
     return redirect('login/')
-<<<<<<< HEAD
 
-=======
->>>>>>> 1b0e38deef21b6f9e7c82277f12da9fb20f27335
 
 app_name = 'dashboard'
 urlpatterns = [
@@ -22,13 +19,9 @@ urlpatterns = [
     path('users/', include('users.urls')), 
     path('users/', include('django.contrib.auth.urls')),
     path('accounts/', include('allauth.urls')),
-<<<<<<< HEAD
     path('dashboard/', include("main.urls")),
-    path('calendarApp/', include("calendarApp.urls"))
-    
-=======
+    path('calendarApp/', include("calendarApp.urls")),
     path('dashboard/', include("main.urls"), name='dashboard'),
-    path('gpa/', include('gpa.urls'))
-,    
->>>>>>> 1b0e38deef21b6f9e7c82277f12da9fb20f27335
+    path('gpa/', include('gpa.urls')),
+  
 ]
